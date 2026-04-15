8a6e12b4f38fb865ea49ec6d79a1c7ec6da01366
Ankon 190 <ankon190@gmail.com>
2026-04-15 15:33:06 +0600

    friend details page design complete

--- src/friendDetails/FriendDetails.jsx
+++ src/friendDetails/FriendDetails.jsx
@@ -1,37 +1,57 @@
 import React from 'react';
+import { FaArchive } from 'react-icons/fa';
+import { RiDeleteBinLine, RiNotificationSnoozeLine } from 'react-icons/ri';
 import { useLoaderData, useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
     const { id } = useParams();
     const friend = useLoaderData();
     return (
-        <div className='bg-base-200'>
-            <div className='container mx-auto grid grid-cols-4 gap-5 pt-10'>
-                <div>
-                    <figure className='rounded-full w-25 h-25 mx-auto overflow-hidden'>
-                        <img
-                            className='w-full h-full'
-                            src={friend.picture} alt="Img" />
-                    </figure>
-                    <div className=''>
-                        <h1 className='text-xl font-bold'>{friend.name}</h1>
-                        <p className='text-[#64748B] text-[14px]'>{friend.days_since_contact}d ago</p>
+        <div className='bg-base-300 py-10 h-screen'>
+            <div className='container mx-auto grid grid-cols-2 md:grid-cols-4 gap-5 pt-10'>
+                <div className='space-y-3'>
+                    <div className='space-y-2 shadow-md rounded-lg bg-white p-5 mx-auto'>
+                        <figure className='rounded-full w-25 h-25 mx-auto overflow-hidden'>
+                            <img
+                                className='w-full h-full'
+                                src={friend.picture} alt="Img" />
+                        </figure>
+                        <div className='text-center'>
+                            <h1 className='text-xl font-bold'>{friend.name}</h1>
+                        </div>
+                        <div className='flex justify-center'>
+                            <span className={friend.status === "overdue" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-red-500" :
+                                friend.status === "almost due" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-[#EFAD44]" :
+                                    friend.status === "on-track" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-[#244D3F]" :
+                                        "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-gray-500"
+                            }>{friend.status}</span>
+                        </div>
+                        <div className='mt-3 flex justify-center'>
+                            {
+                                friend.tags.map((tag, index) => (
+                                    <span key={index} className="badge bg-[#CBFADB] text-[12px] text-[#244D3F] font-semibold mr-2 rounded-2xl">{tag}</span>
+                                ))
+                            }
+                        </div>
+                        <div className='text-center space-y-3'>
+                            <h3 className='text-lg italic font-semibold text-gray-500'>
+                                {friend.bio}
+                            </h3>
+                            <p className='text-[18px] text-gray-500'>Preffered: {friend.email}</p>
+                        </div>
                     </div>
-                    <div className='mt-3'>
-                        {
-                            friend.tags.map((tag, index) => (
-                                <span key={index} className="badge bg-[#CBFADB] text-[12px] text-[#244D3F] font-semibold mr-2 rounded-2xl">{tag}</span>
-                            ))
-                        }
+                    <div className='bg-white rounded-lg border border-gray-200 p-2'>
+                        <h4 className='flex justify-center items-center text-xl font-semibold'><RiNotificationSnoozeLine className='mr-2' />Snooze 2 Weeks</h4>
                     </div>
-                    <span className={friend.status === "overdue" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-red-500" :
-                        friend.status === "almost due" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-[#EFAD44]" :
-                            friend.status === "on-track" ? "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-[#244D3F]" :
-                                "badge text-[12px] text-white font-semibold mr-2 rounded-2xl bg-gray-500"
-                    }>{friend.status}</span>
+                    <div className='bg-white rounded-lg border border-gray-200 p-2'>
+                        <h4 className='flex justify-center items-center text-xl font-semibold'><FaArchive className='mr-2' />Archive</h4>
+                    </div>
+                    <div className='bg-white rounded-lg border border-gray-200 p-2'>
+                        <h4 className='flex justify-center items-center text-xl font-semibold'><RiDeleteBinLine className='mr-2' />Delete</h4>
+                    </div>
                 </div>
-                <div className='md:col-span-3'>
-                    <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                <div className='col-span-2 md:col-span-3 space-y-10'>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 mx-10 md:mx-0'>
                         <div className='bg-white rounded-xl py-5 text-center'>
                             <h1 className='text-[28px] font-semibold text-[#244D3F]'>{friend.days_since_contact}</h1>
                             <p className='text-[16px] opacity-80 text-[#64748B]'>Days Science Contact</p>
@@ -45,13 +65,38 @@ const FriendDetails = () => {
                             <p className='text-[16px] opacity-80 text-[#64748B]'>Next Due</p>
                         </div>
                     </div>
-                    <div className='p-5'>
+                    <div className='p-5 my-7 mx-10 md:mx-0 rounded-xl bg-white'>
                         <div className='flex justify-between'>
-                            <h1 className='text-[#244D3F] font-semibold'>Relationship Goals</h1>
-                            <button className='px-3 rounded-md border border-gray-200'>Edit</button>
+                            <h1 className='text-[#244D3F] font-semibold text-[18px]'>Relationship Goals</h1>
+                            <button className='px-3 rounded-md border border-gray-200 bg-base-400'>Edit</button>
                         </div>
                         <p className='text-gray-400'>Connected every <span className='font-bold text-black'>30 days</span></p>
                     </div>
+                    <div className='p-5 my-5 rounded-xl text-2xl bg-white space-y-7'>
+                        <h1 className='text-[#244D3F] font-semibold'>Quick Check-In</h1>
+                        <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                            <button className='flex flex-col justify-center items-center p-5 space-y-3 bg-base-200 rounded-xl border border-gray-200 cursor-pointer'>
+                                <img
+                                    className='w-10'
+                                    src="/src/assets/call.png" alt="" />
+                                <h3 className='text-[20px] font-semibold'>Call</h3>
+                            </button>
+                            <button className='flex flex-col justify-center items-center p-5 space-y-3 bg-base-200 rounded-xl border border-gray-200 cursor-pointer'>
+                                <img
+                                    className='w-10'
+                                    src="/src/assets/text.png" alt="" />
+                                <h3 className='text-[20px] font-semibold'>Text</h3>
+                            </button>
+                            <button className='flex flex-col justify-center items-center p-5 space-y-3 bg-base-200 rounded-xl border border-gray-200 cursor-pointer'>
+                                <img
+                                    className='w-10'
+                                    src="/src/assets/video.png" alt="" />
+                                <h3 className='text-[20px] font-semibold'>Video</h3>
+                            </button>
+
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
